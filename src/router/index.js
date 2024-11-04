bac834232da389738ebb789c5fe18c0d81158294
Miguel Carvajal <m.angel9106@gmail.com>
2024-11-04 11:20:51 -0300

    subiendo desafio 1 modulo7

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/productos',
+    name: 'productos',
+    component: () => import(/* webpackChunkName: "productos" */ '../views/Productos.vue'),
+    props:{
+      producto:{
+        nombre: "Desayunos",
+        descripcion: "Contamos con desayunos nutritivos y deliciosos",
+        precio: 2000,
+        cantidad: "Para dos personas"
+      }
+    }
+  },
+  {
+    path: '/contacto',
+    name: 'contacto',
+    component: () => import(/* webpackChunkName: "contacto" */ '../views/Contacto.vue'),
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Contacto.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    <h1>
+        Vista de Contacto
+    </h1>
+
+    <div>
+        <router-link class="btn btn-success" to="/">Volver al inicio</router-link>
+    </div>
+</template>
